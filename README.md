4eeec0e1aa591dfdae463e4fca16ed126d3d0cd2
tanamet.tpk <tanamet.tpk@gmail.com>
2019-08-11 14:08:08 +0700

    + readme and cors

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-code-controller
+# Code controller microservices
+## ENV NEED
+- APP_HOST for application server host name
+- TEMPLATE_HOST for template server host name
+- GENERATOR_HOST for generator server host name
+- AWT_SECRET for awt secret file
--- config/express.js
+++ config/express.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const http = require("http")
 const middleware = require("./middlewares")
+const cors = require("cors")
 
 module.exports = function(){
 
@@ -8,7 +9,7 @@ module.exports = function(){
 	const server = http.createServer(app);
 
 	app.use(express.json());
-	
+	app.use(cors());
 	app.use(middleware.customResponses);
 	
 	app.use(middleware.logger);
