e22cca02e428f111a5862fd96929ec666f6f2a1e
tanamet.tpk <tanamet.tpk@gmail.com>
2019-09-04 14:47:33 +0700

    + ClassGenerator class

--- app/libs/programming/ClassGenerator.js
+++ app/libs/programming/ClassGenerator.js
@@ -1,7 +1,123 @@
+const Template = require('../engine/FileTemplate')
+
 class ClassGenerator {
 
     constructor(database){
         this.database = database
+        this.name = this.capitalize(database.name)
+        this.path = __dirname
+        this.keys = {databaseName: database.name , databaseNameCapital: this.name}
+    }
+
+    generate(){
+
+        this.setupAttributes()
+        this.setupHahableAttributes()
+        this.setupUpdableAttributes()
+        
+        let template = new Template(`${this.path}/prototypes/class/class.pt`)
+
+        return template.generate(this.keys)
+    }
+
+    setupAttributes(){
+
+        this.keys = {
+            ...this.keys,
+            attributes: this.database.attributes.filter((att) => {
+                return att.isGet
+            }).map((att) => att.name)
+        }
+
+    }
+
+    setupUpdableAttributes(){
+
+        let canAttributes = this.database.attributes.filter((att) => {
+            return att.isUpdate 
+        })
+
+        let updatable_attributes = canAttributes.map((att) => att.name)
+
+        let encryptTemplate = new Template(`${this.path}/prototypes/class/validateFunction.pt`)
+        let updateVarTemplate = new Template(`${this.path}/prototypes/class/updateVar.pt`)
+        let updateSectionTemplate = new Template(`${this.path}/prototypes/class/updateSection.pt`)
+        let update_code = ""
+
+        canAttributes.map((att) => {
+
+            let update_var = ""
+
+            if (att.isEncrypt){
+
+                update_var = encryptTemplate.generate({
+                    att: att.name,
+                    Att: this.capitalize(att.name),
+                    databaseName: "current"
+                })
+
+            }else{
+
+                update_var = updateVarTemplate.generate({
+                    att: att.name
+                })
+
+            }
+
+            update_code += updateSectionTemplate.generate({
+                att: att.name,
+                update_var
+            })
+
+        })
+
+
+        this.keys = {
+            ...this.keys,
+            updatable_attributes,
+            update_code,
+        }
+
+    }
+
+    setupHahableAttributes(){
+
+        let hashable = this.database.attributes.filter((att) => {
+            return att.isEncrypt
+        })
+
+        if ( hashable.length < 1 ) return
+
+        let hashVar = hashable[0].name
+
+        let template = new Template(`${this.path}/prototypes/class/validateFunction.pt`)
+        let hashable_attributes = template.generate({
+            databaseName: this.database.name,
+            Att: this.capitalize(hashVar),
+            att: hashVar
+        })
+
+        // extension
+
+        let auth_var = this.database.attributes.filter((att) => {
+            return att.isAuth
+        }).map((att) => att.name)
+
+        template = new Template(`${this.path}/prototypes/options/class/validate.pt`)
+        let extension = template.generate({
+            databaseName: this.database.name,
+            encrypt_var_capital: this.capitalize(hashVar),
+            encrypt_var: hashVar,
+            auth_var
+        })
+
+        this.keys = {
+            ...this.keys,
+            hashable_attributes,
+            extension,
+            export: [`validate${this.capitalize(hashVar)}`]
+        }
+
     }
 
     capitalize(text) {
--- test/unit/ClassGenerator.spec.js
+++ test/unit/ClassGenerator.spec.js
@@ -10,10 +10,11 @@ const Attribute = require('../../app/libs/programming/Attribute')
 describe('ClassGenerator', () => {
 
     let generator
+    let db
 
     beforeEach(()=>{
 
-        let db = new DatabaseGenerator("name")
+        db = new DatabaseGenerator("application")
         generator = new ClassGenerator(db)
 
     })
@@ -22,7 +23,55 @@ describe('ClassGenerator', () => {
 
         it('should have name from database with capitalize', () => {
             
-            expect(generator.name).to.equal("Name")
+            expect(generator.name).to.equal("Application")
+
+        });
+
+    });
+
+    describe('generate', () => {
+
+        it('should be right format', () => {
+            
+            db.addAttribute(new Attribute('name'))
+            db.addAttribute(new Attribute('created_at'))
+            db.addAttribute(new Attribute('created_by'))
+
+            let content = fs.readFileSync(__dirname + '/../data/class.txt' , 'utf8')
+
+            expect(generator.generate()).to.equal(content)
+
+        });
+
+        it('should have validate method', () => {
+            
+            db = new DatabaseGenerator("user")
+            generator = new ClassGenerator(db)
+
+            let email = new Attribute('email')
+            let name = new Attribute('name')
+            let password = new Attribute('password')
+            let pt = new Attribute('provider_type')
+            let pi = new Attribute('provider_id')
+
+            email.id().auth()
+            name.auth()
+            password.encrypt().disableGet()
+            pt.disableUpdate()
+            pi.disableUpdate()
+            pi.disableGet()
+
+
+            db.addAttribute(name)
+            db.addAttribute(email)
+            db.addAttribute(password)
+            db.addAttribute(pt)
+            db.addAttribute(new Attribute('provider_token'))
+            db.addAttribute(pi)
+
+            let content = fs.readFileSync(__dirname + '/../data/classAuth.txt' , 'utf8')
+
+            expect(generator.generate()).to.equal(content)
 
         });
 
