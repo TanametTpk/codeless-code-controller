e6df324fe4a56226d32ff3b6d78cab598966f7a6
Tui Tpk <tanamet.tpk@gmail.com>
2019-12-10 16:44:22 +0700

    + route for new generator version

--- app/routes/generator.js
+++ app/routes/generator.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.post("/", generator.generate);
 router.post("/inplace", generator.generateFromDb);
-
+router.post("/v2", generator.generateFromDbV2);
 
 module.exports = router;
